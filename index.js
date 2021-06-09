91befecf768bdaef8ee1a07b412c12c44dd49170
RichardEstime <richardjestime@yahoo.com>
2021-06-09 00:42:11 -0400

    commit tree 2ad459949bdf67f71984bad646ce5a13869dc1a2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+
+function superbowlWin(records){
+    let found = records.find(function(record){
+       return record.result === 'W';
+    })
+}
+
+function superbowlWin(record){
+    return('1969')
+}
+
